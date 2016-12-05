3266ca71d06c45c38c29d68fae1d1420ce380b3d
Naresh.Kumar <Naresh.Kiroriwal@gmail.com>
2016-12-05 11:50:30 +0530

    Add FOrgot Password

--- SampleWebSite/Login.aspx.designer.cs
+++ SampleWebSite/Login.aspx.designer.cs
@@ -29,5 +29,14 @@ namespace SampleWebSite {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Login Login1;
+        
+        /// <summary>
+        /// PasswordRecovery1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.PasswordRecovery PasswordRecovery1;
     }
 }
